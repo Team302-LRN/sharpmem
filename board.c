41a23b19b2e4cfe84050f76d2efb0ca2b7910e09
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-11 12:08:04 -0400

    Fixed compile issues

--- board.c
+++ board.c
@@ -5,27 +5,27 @@
   Sets pin equal to val.
   val should be 0 or 1
 */
-void set_cs(uint8_t val)
+void set_ss(uint8_t val)
 {
     PORTBbits.RB5 = val;
 }
 
-void set_d1(uint8_t val)
+void set_mosi(uint8_t val)
 {
     PORTBbits.RB3 = val;
 }
 
-void set_sck(uint8_t val)
+void set_clk(uint8_t val)
 {
     PORTBbits.RB2 = val;
 }
 
 /*
-  Configure CS pin as output.
-  CS is RB5, which has no analog select,
+  Configure SS pin as output.
+  SS is RB5, which has no analog select,
   so no need to disable.
 */
-void cs_config_out(void)
+void ss_config_out(void)
 {
   TRISBbits.TRISB5 = 0;
 }
@@ -34,7 +34,7 @@ void cs_config_out(void)
   Configure D1 pin as output.
   D1 is on RB3. Disable analog and select digital out.
 */
-void d1_config_out(void)
+void mosi_config_out(void)
 {
   ANSBbits.ANSB3 = 0;
   TRISBbits.TRISB3 = 0;
@@ -44,7 +44,7 @@ void d1_config_out(void)
   Configure SCK pin as output.
   SCK is on RB2. Disable analog and select digital out.
 */
-void sck_config_out(void)
+void clk_config_out(void)
 {
   ANSBbits.ANSB2 = 0;
   TRISBbits.TRISB3 = 0;
