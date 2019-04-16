196fce2f83f849427b57a82ca08f586314e2d16c
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-04-16 10:46:07 -0400

    Trying to delete cached files

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=disp_spi.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/disp_spi.X.production.hex
+CND_ARTIFACT_NAME_default=sharpmem.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/sharpmem.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=dispspi.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/dispspi.x.tar
+CND_PACKAGE_NAME_default=sharpmem.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/sharpmem.tar
